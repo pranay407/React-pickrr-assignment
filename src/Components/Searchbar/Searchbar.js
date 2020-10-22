76dc770773b2aba7ff37c31d9243e8540d29f0c6
Pranay Gaikwad <pranaygaikwad407@gmail.com>
2020-10-22 12:05:02 +0530

    search functionality added

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -1,41 +1,91 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import { Input } from 'antd';
 import './Searchbar.css'
+import { Card, Space, Typography } from 'antd';
+import { StarOutlined } from '@ant-design/icons';
+import LinesEllipsis from "react-lines-ellipsis";
 
 const { Search } = Input;
+const { Title } = Typography;
 // const onSearch = value => console.log(value);
 
 
 
 
-function Searchbar(){
+function Searchbar() {
     const [ser, setSearch] = useState("");
-    // const [query, setQuery] = useState("");
-    const handleBeers = (e) =>{
-        setSearch(e.target.value);
-        console.log(e.target.value);
+    const [query, setQuery] = useState("");
+    const handleBeers = (e) => {
+        setQuery(e.target.value);
+        // console.log(e.target.value);
+        console.log(query);
     }
 
-    // const HandleSearch =() =>{
-    //     useEffect(() =>{
-    //         fetch('https://api.punkapi.com/v2/beers?beer_name=${ser}')
-    //             .then(res => res.json())
-    //             .then(data => setSearch(data))
+    const HandleSearch = (query) =>{
+        
+            fetch(`https://api.punkapi.com/v2/beers?beer_name=${query}`)
+                .then(res => res.json())
+                .then((data) =>{
+                    setSearch(data);
+                })
                 
-    //     })
-    // }
     
-    return(
+        
+    }
+    
+
+
+
+    return (
         <>
-            <Search 
-            className="searchStyle" 
-            placeholder="search for beer..." 
-            enterButton="search" 
-            size="large" 
-            value={ser}
-            // onSearch={HandleSearch}
-            onChange={(e) => handleBeers(e)}
-             />
+            <Search
+                className="searchStyle"
+                placeholder="search for beer..."
+                enterButton="search"
+                size="large"
+                value={query}
+                onSearch={HandleSearch}
+                onChange={(e) => handleBeers(e)}
+            />
+            <div className="card">
+            {ser 
+                ? ser.map((item) => {
+                    return (
+                        <div style={{ backgroundColor: 'white', padding: '10px' }}>
+
+
+                            <Card style={{ height: '70%' }} key={item.id}
+                                hoverable>
+                                <StarOutlined
+                                    className="iconStyle"
+
+                                />
+                                <Space>
+                                    <img src={item.image_url} alt={item.name} style={{ width: 100, height: 300 }}></img>
+                                    <Space direction="vertical">
+                                        <Title level={3}>{item.name}</Title>
+                                        <LinesEllipsis
+                                            text={item.description}
+                                            maxLine="3"
+                                            ellipsis="..."
+                                            trimRight
+                                            basedOn="letters"
+                                        />
+                                    </Space>
+
+                                </Space>
+                            </Card>
+
+
+                        </div>
+                    )
+                })
+                :null}
+            
+
+        </div>
+
+
         </>
     );
 }
